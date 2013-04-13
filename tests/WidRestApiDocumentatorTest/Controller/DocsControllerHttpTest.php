3b56786608d23cf01ec28b916ccd0567b9edfb8c
widmogrod <widmogrod@gmail.com>
2013-04-13 14:46:55 +0200

    small change

---
+++ tests/WidRestApiDocumentatorTest/Controller/DocsControllerHttpTest.php
@@ -0,0 +1,39 @@
+<?php
+namespace WidRestApiDocumentatorTest\Controller;
+
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+
+class DocsController extends AbstractHttpControllerTestCase
+{
+    protected $traceError = true;
+
+    public function setUp()
+    {
+        $this->setApplicationConfig(
+            include 'tests/config/application.config.php'
+        );
+        parent::setUp();
+    }
+
+    public function testListAction()
+    {
+        $this->dispatch('/rest-api-docs');
+        $this->assertResponseStatusCode(200);
+        $this->assertModuleName('WidRestApiDocumentator');
+        $this->assertActionName('list');
+        $this->assertControllerName('WidRestApiDocumentator\Controller\Docs');
+        $this->assertControllerClass('DocsController');
+        $this->assertMatchedRouteName('rest-api-docs');
+    }
+
+    public function testShowAction()
+    {
+        $this->dispatch('/rest-api-docs/simple');
+        $this->assertResponseStatusCode(200);
+        $this->assertModuleName('WidRestApiDocumentator');
+        $this->assertActionName('show');
+        $this->assertControllerName('WidRestApiDocumentator\Controller\Docs');
+        $this->assertControllerClass('DocsController');
+        $this->assertMatchedRouteName('rest-api-docs/show');
+    }
+}
