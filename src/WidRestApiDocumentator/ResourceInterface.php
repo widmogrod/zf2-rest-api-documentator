379a8caee80fc706be3f1ce158a1149179f959f1
widmogrod <widmogrod@gmail.com>
2013-04-13 15:30:11 +0200

    add show template

--- src/WidRestApiDocumentator/ResourceInterface.php
+++ src/WidRestApiDocumentator/ResourceInterface.php
@@ -6,6 +6,10 @@ interface ResourceInterface
     public function setMethod($value);
     public function getMethod();
     public function setParams(ParamSetInterface $value);
+
+    /**
+     * @return ParamSetInterface
+     */
     public function getParams();
     public function setDescription($value);
     public function getDescription();
