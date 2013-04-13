00abab8fd5f245ec20d7c229112fcedc7be35bcd
widmogrod <widmogrod@gmail.com>
2013-04-13 19:46:33 +0200

    add partial to configuration

--- config/module.config.php
+++ config/module.config.php
@@ -41,6 +41,7 @@ return array(
             'wid-rest-api-documentator/docs/layout' => __DIR__ . '/../view/layout/layout.phtml',
             'wid-rest-api-documentator/docs/list' => __DIR__ . '/../view/docs/list.phtml',
             'wid-rest-api-documentator/docs/show' => __DIR__ . '/../view/docs/show.phtml',
+            'wid-rest-api-documentator/partials/params' => __DIR__ . '/../view/partials/params.phtml',
         ),
     ),
 );
