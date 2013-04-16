4ad9d3834253dce80d131e18be9990927b52869c
widmogrod <widmogrod@gmail.com>
2013-04-16 19:31:01 +0200

    prepare to sending debug info

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,25 @@
 (function($){
     $('.api-method').on('click', function(){
-        $(this).siblings('.toggable').toggleClass('show')
+        $(this).siblings('.toggable').toggleClass('show');
+    });
+    $('.test-request').on('click', function(){
+        var parent = $(this).parents('.entrypoint');
+        var res = parent.find('.response');
+
+        $.ajax({
+            'url': $(this).data('href'),
+            'dataType': 'html',
+            'success': function(data) {
+                parent.find('.toggable').toggleClass('show');
+                parent.find('.toggable-response').toggleClass('show');
+                res.val(data);
+            },
+            'error' : function(data) {
+                parent.find('.toggable').toggleClass('show');
+                parent.find('.toggable-response').toggleClass('show');
+                res.val(data);
+            }
+        });
+
     });
 })(jQuery);
