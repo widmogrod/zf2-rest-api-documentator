e13d5e03d590776a1827e371f4a9ac566fa4e594
widmogrod <widmogrod@gmail.com>
2013-04-17 22:54:42 +0200

    update api config

--- config/api.config.php
+++ config/api.config.php
@@ -1,27 +1,35 @@
 <?php
 return array(
-    'router' => array(
-        'routes' => array(
-            'rest-api-docs' => array(
-                'type' => 'Literal',
-                'options' => array(
-                    'route' => '/rest-api-docs',
-                    'defaults' => array(
-                        'controller' => 'WidRestApiDocumentator\Controller\Docs',
-                        'action' => 'list',
+    // Configuration namespace within this module looking for data
+    'zf2-rest-api-documentator' => array(
+        // Contains collection of documentation descriptions.
+        'docs' => array(
+            // Namespace of module within REST API description resides. Must be unique per module.
+            'zf2-rest-api-documentator' => array(
+                'name' => 'widmogrod/zf2-rest-api-documentator',
+                'version' => '1.0.0',
+                'baseUrl' => 'http://127.0.0.1:8081/api',
+                // Strategy is way, in which this configuration will be interpreted.
+                'strategy' => 'standard',
+                // General description for common thing in module, to skip redundancy
+                'general' => array(
+                    'params' => array(
+                        'id' => array(
+                            'type' => 'string',
+                            'required' => true,
+                            'description' => 'Resource identification'
+                        ),
                     ),
                 ),
-                'may_terminate' => true,
-                'child_routes' => array(
-                    'show' => array(
-                        'type' => 'Segment',
-                        'options' => array(
-                            'route' => '/:id',
-                            'constraints' => array(
-                                'id' => '[^/ ]+',
-                            ),
-                            'defaults' => array(
-                                'action' => 'show',
+                // REST API Endpoings, here you describing your API
+                'resources' => array(
+                    'POST: /request/<id>/<endpoint>' => array(
+                        'description' => 'Perform request for given API <id> - <endpoint>',
+                        'params' => array(
+                            'endpoint' => array(
+                                'type' => 'string',
+                                'required' => true,
+                                'description' => 'Endpoint identification'
                             ),
                         ),
                     ),
