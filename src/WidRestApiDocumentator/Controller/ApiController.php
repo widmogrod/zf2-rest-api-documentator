fe60c46a558c081c727024c32337fefcf09b7490
widmogrod <widmogrod@gmail.com>
2013-04-20 13:16:41 +0200

    api controller

--- src/WidRestApiDocumentator/Controller/ApiController.php
+++ src/WidRestApiDocumentator/Controller/ApiController.php
@@ -1,8 +1,32 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: gabriel
- * Date: 19.04.2013
- * Time: 18:47
- * To change this template use File | Settings | File Templates.
- */
+namespace WidRestApiDocumentator\Controller;
+
+use Zend\Http\PhpEnvironment\Request;
+use Zend\Http\PhpEnvironment\Response;
+use Zend\Mvc\Controller\AbstractActionController;
+
+class ApiController extends AbstractActionController
+{
+    /**
+     * @return \WidRestApiDocumentator\Service\Api
+     */
+    public function getApiService()
+    {
+        return $this->getServiceLocator()->get('WidRestApiDocumentator\Service\Api');
+    }
+
+    public function doAction()
+    {
+        /** @var $rq Request */
+        $rq = $this->getRequest();
+        $id = $this->params('id');
+        $endpoint = $this->params('endpoint');
+
+        $service = $this->getApiService();
+        $result = $service->perform($id, $endpoint, $rq->getPost());
+
+        $response = new Response();
+        $response->setContent($result);
+        return $response;
+    }
+}
