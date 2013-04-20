f2aee83ec583353c5ff92e3226f5cab16ef565b0
widmogrod <widmogrod@gmail.com>
2013-04-20 16:51:42 +0200

    change ajax call to api

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,17 +6,21 @@
         var parent = $(this).parents('.entrypoint');
         var res = parent.find('.response');
 
+        var data = parent.find(':input').serializeArray();
+
         $.ajax({
-            'url': $(this).data('href'),
+            'url': $(this).data('api-url'),
+            'type': 'post',
+            'data': data,
             'dataType': 'html',
             'success': function(data) {
-                parent.find('.toggable').toggleClass('show');
-                parent.find('.toggable-response').toggleClass('show');
+                parent.find('.toggable').addClass('show');
+                parent.find('.toggable-response').addClass('show');
                 res.val(data);
             },
             'error' : function(data) {
-                parent.find('.toggable').toggleClass('show');
-                parent.find('.toggable-response').toggleClass('show');
+                parent.find('.toggable').addClass('show');
+                parent.find('.toggable-response').addClass('show');
                 res.val(data);
             }
         });
