1d47bfb55d21b8b97b5fd54e0443606bc6678bee
Gabriel Habryn <gabriel.habryn@schibsted.pl>
2013-04-26 15:37:51 +0200

    update BodyInterface

--- src/WidRestApiDocumentator/BodyInterface.php
+++ src/WidRestApiDocumentator/BodyInterface.php
@@ -1,8 +1,6 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: gabrielhabryn
- * Date: 26.04.2013
- * Time: 14:14
- * To change this template use File | Settings | File Templates.
- */
+namespace WidRestApiDocumentator;
+
+interface BodyInterface {
+    public function toString();
+}
