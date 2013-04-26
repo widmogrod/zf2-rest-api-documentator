c730a974d6a9159655a570c6e274f691f2ee02e2
Gabriel Habryn <gabriel.habryn@schibsted.pl>
2013-04-26 15:39:15 +0200

    update ResourceInterface

--- src/WidRestApiDocumentator/ResourceInterface.php
+++ src/WidRestApiDocumentator/ResourceInterface.php
@@ -5,20 +5,31 @@ interface ResourceInterface
 {
     public function setMethod($value);
     public function getMethod();
-    public function setQueryParams(ParamSetInterface $value);
 
+    public function setQueryParams(ParamSetInterface $value);
     /**
      * @return ParamSetInterface
      */
     public function getQueryParams();
-    public function setUrlParams(ParamSetInterface $value);
 
+    public function setUrlParams(ParamSetInterface $value);
     /**
      * @return ParamSetInterface
      */
     public function getUrlParams();
+
+    public function setHeaders(HeaderSetInterface $headers);
+    public function getHeaders();
+
+    public function setBody(BodyInterface $body);
+    /**
+     * @return BodyInterface
+     */
+    public function getBody();
+
     public function setDescription($value);
     public function getDescription();
+
     public function setUrl($value);
     public function getUrl();
 }
